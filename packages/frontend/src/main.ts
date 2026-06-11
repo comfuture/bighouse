4fc8c9bd94bfc838f351255ae6939520e337abb0
comfuture <comfuture@gmail.com>
2026-06-11 15:30:18 +0900

    feat: add elastic game UI feedback

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -2,6 +2,8 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import ui from "@nuxt/ui/vue-plugin";
 import App from "./App.vue";
+import { installElasticPointerFeedback } from "./elastic-hover";
 import { router } from "./router";
 
+installElasticPointerFeedback();
 createApp(App).use(router).use(ui).mount("#app");
